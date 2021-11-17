24816eaea6bc0b18953e6b04019acab029081a64
radwanromy <radwanromy@gmail.com>
2021-11-17 11:53:57 +0600

    VG DB AG 010

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  public sort!: string;
   constructor() { }
 
   ngOnInit(): void {
