99272e055b482f300cedece2bd412bd6f0f616e5
radwanromy <radwanromy@gmail.com>
2021-11-17 12:59:15 +0600

    VG DB AG 019

--- src/app/models.ts
+++ src/app/models.ts
@@ -17,3 +17,32 @@ export interface Game {
   export interface APIResponse<T> {
     results: Array<T>;
 }
+interface Genre {
+    name: string;
+  }
+  
+  interface ParentPlatform {
+    platform: {
+      name: string;
+    };
+  }
+  
+  interface Publishers {
+    name: string;
+  }
+  
+  interface Rating {
+    id: number;
+    count: number;
+    title: string;
+  }
+  
+  interface Screenshots {
+    image: string;
+  }
+  
+  interface Trailer {
+    data: {
+      max: string;
+    };
+  }
