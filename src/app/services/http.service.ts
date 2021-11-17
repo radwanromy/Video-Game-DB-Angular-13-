25744f6685fe54de73a265f54a64773eb79f36f9
radwanromy <radwanromy@gmail.com>
2021-11-17 16:22:26 +0600

    VG DB AG 030

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -22,32 +22,8 @@ export class HttpService {
       params = new HttpParams().set('ordering', ordering).set('search', search);
     }
 
-    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
+    return this.http.get<APIResponse<Game>>('${env.BASE_URL}/games', {
       params: params,
     });
-  }
-
-  getGameDetails(id: string): Observable<Game> {
-    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
-    const gameTrailersRequest = this.http.get(
-      `${env.BASE_URL}/games/${id}/movies`
-    );
-    const gameScreenshotsRequest = this.http.get(
-      `${env.BASE_URL}/games/${id}/screenshots`
-    );
-
-    return forkJoin({
-      gameInfoRequest,
-      gameScreenshotsRequest,
-      gameTrailersRequest,
-    }).pipe(
-      map((resp: any) => {
-        return {
-          ...resp['gameInfoRequest'],
-          screenshots: resp['gameScreenshotsRequest']?.results,
-          trailers: resp['gameTrailersRequest']?.results,
-        };
-      })
-    );
   }
 }
