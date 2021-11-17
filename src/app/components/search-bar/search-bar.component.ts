32cec0e9e32f549c0083e8f70e6411d37771f2ce
radwanromy <radwanromy@gmail.com>
2021-11-17 11:39:16 +0600

    VG DB AG 007

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+  onSubmit(form: NgForm){
+    this.router.navigate(['search',form.value.search]);
   }
 
 }
