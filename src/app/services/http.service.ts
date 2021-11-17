b442cc0855194eecdacf745f08a96e3c2e1e8ed0
radwanromy <radwanromy@gmail.com>
2021-11-17 13:01:27 +0600

    VG DB AG 020

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { APIResponse, Game } from '../models';
 
 @Injectable({
   providedIn: 'root'
