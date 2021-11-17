48f6268fb96f3cbcd0e91d845f564e5804928a6f
radwanromy <radwanromy@gmail.com>
2021-11-17 12:58:28 +0600

    VG DB AG 017

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,3 +1,15 @@
 export interface Game {
-    
-}
+    background_image: string;
+    name: string;
+    released: string;
+    metacritic_url: string;
+    website: string;
+    description: string;
+    metacritic: number;
+    genres: Array<Genre>;
+    parent_platforms: Array<ParentPlatform>;
+    publishers: Array<Publishers>;
+    ratings: Array<Rating>;
+    screenshots: Array<Screenshots>;
+    trailers: Array<Trailer>;
+  }
